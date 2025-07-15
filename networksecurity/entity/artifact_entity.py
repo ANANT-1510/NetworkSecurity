e9c212535b6eff385f2b36112623546549197965
ANANT-1510 <163185234+ANANT-1510@users.noreply.github.com>
2025-07-16 03:23:24 +0530

    Data Transformation Completed

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -4,3 +4,19 @@ from dataclasses import dataclass
 class DataIngestionArtifact:
   trained_file_path:str
   test_file_path:str
+
+@dataclass
+class DataValidationArtifact:
+  validation_status:bool
+  valid_train_file_path:str
+  valid_test_file_path:str
+  invalid_train_file_path:str
+  invalid_test_file_path:str
+  drift_report_file_path:str
+
+
+@dataclass
+class DataTransformationArtifact:
+  tranformed_object_file_path:str
+  tranformed_train_file_path:str
+  tranformed_test_file_path:str
